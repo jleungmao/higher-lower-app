3eb61e8708da89b1214b84309b696113545dc70b
Joey Leung <jleung147@gmail.com>
2022-01-01 16:44:48 -0500

    Update AddChannel.jsx

--- frontend/src/pages/AddChannel.jsx
+++ frontend/src/pages/AddChannel.jsx
@@ -4,15 +4,21 @@ import axios from 'axios';
 
 function AddChannel() {
     const [channelName, setChannelName] = useState("");
+    const [channelURL, setChannelURL] = useState("");
     const [resultsExist, setResultsExist] = useState(false);
     const [channelList, setChannelList] = useState([]);
 
+
+
     const handleChannelNameChange = (event) => {
         setChannelName(event.target.value);
-        // console.log(event.target.value);
     };
 
-    const handleFormSubmit = (event) => {
+    const handleChannelURLChange = (event) => {
+        setChannelURL(event.target.value);
+    };
+
+    const handleChannelNameForm = (event) => {
         event.preventDefault();
         if (channelName.length === 0)
             return;
@@ -31,6 +37,24 @@ function AddChannel() {
 
     };
 
+    const handleChannelURLForm = (event) => {
+        event.preventDefault();
+        if (channelURL.length === 0)
+            return;
+        axios.get('http://localhost:8080/get-channel-by-url', { params: { channelURL: channelURL } })
+            .then(result => {
+                if (result.data.length > 0) {
+                    setResultsExist(true);
+                    setChannelList(result.data);
+                    console.log(result.data);
+                } else {
+                    setResultsExist(false);
+                }
+            }).catch(err => {
+                console.log(err);
+            });
+    };
+
     const displayResults = () => (
         <div className='section-box'>
             {channelList.map(channelInfo => {
@@ -46,11 +70,18 @@ function AddChannel() {
         <div>
             <div className="section-box">
                 <h1>Search for Channel</h1>
-                <form className="channel-form" onSubmit={handleFormSubmit}>
-                    <label className='channel-form-label' htmlFor="channelName">Channel Name:</label>
-                    <input type="text" value={channelName} onChange={handleChannelNameChange} />
-                    <button type='submit'>Search</button>
-                </form>
+                <div className='forms-box'>
+                    <form className="channel-form" onSubmit={handleChannelNameForm}>
+                        <label className='channel-form-label' htmlFor="channelName">By Channel Name:</label>
+                        <input type="text" value={channelName} onChange={handleChannelNameChange} />
+                        <button type='submit'>Search</button>
+                    </form>
+                    <form className="channel-form" onSubmit={handleChannelURLForm}>
+                        <label className='channel-form-label' htmlFor="channelName">By Channel URL:</label>
+                        <input type="text" value={channelURL} onChange={handleChannelURLChange} />
+                        <button type='submit'>Search</button>
+                    </form>
+                </div>
             </div>
             {resultsExist ? displayResults() : ''}
         </div>
